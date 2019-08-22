439d51f669fbd6e2f9becab5c512596e6e6f42ac
Chinedu <neduvinci@gmail.com>
2019-08-22 12:01:35 +0100

    started refactoring of task 2

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -60,6 +60,8 @@ class Person {
     }
 }
   
+//Testing code
+
 //   let first = new Person('John', 45);
 //   console.log(first.greet());
 //   first.eatEdible('cookie');
@@ -68,3 +70,72 @@ class Person {
 //   console.log(first.stomach);
 //   first.poop();
 //   console.log(first.stomach);
+
+
+/*
+  TASK 2
+
+  - Build a Car constructor that takes model name and make.
+  - Give cars the ability to drive a distance.
+  - By driving a car, the distance driven should be added to an "odometer" property.
+  - Give cars the ability to crash.
+  - A crashed car can't be driven any more. Attempts return a string "I crashed at x miles!", x being the miles in the odometer.
+  - Give cars the ability to be repaired.
+  - A repaired car can be driven again.
+*/
+
+
+//Native solution
+
+// function Car(model, make) {
+//     this.car_model = model;
+//     this.car_make = make;
+//     this.odometer = 0;
+//     this.canBeDriven = true;
+//   }
+  
+//   Car.prototype.drive = function(distance) {
+//     if(this.canBeDriven) {
+//      return this.odometer += distance;
+//     }
+//     return `I crashed at ${this.odometer} miles.`;  
+//   }
+  
+//   Car.prototype.crash = function() {
+//     return this.canBeDriven = false;
+//   }
+  
+//   Car.prototype.repair = function() {
+//     return this.canBeDriven = true;
+//   }
+
+
+//Refactoring use class
+
+class Car {
+    constructor(model, make) {
+        this.car_model = model;
+        this.car_make = make;
+        this.odometer = 0;
+        this.canBeDriven = true;
+    }
+
+    drive() {
+        if(this.canBeDriven) {
+            return this.odometer += distance;
+        }
+        
+    }
+}
+
+  //Testing code
+
+  // let myfirst = new Car('nissan', 'sunny');
+  // console.log(myfirst.drive(120));
+  // console.log(myfirst.drive(120));
+  // myfirst.crash();
+  // console.log(myfirst.drive(120));
+  // myfirst.repair();
+  // console.log(myfirst.drive(120));
+  // myfirst.crash();
+  // console.log(myfirst.drive(120));
